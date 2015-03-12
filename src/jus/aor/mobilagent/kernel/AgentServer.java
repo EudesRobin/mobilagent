ec84b7b7a070684fdc45b78e76423abb36c93bd5
Eudes Robin <eudes.robin@gmail.com>
2015-03-12 20:00:09 +0100

    init infra

---
+++ src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package jus.aor.mobilagent.kernel;
+
+/**
+ * @author eudes
+ *
+ */
+public class AgentServer extends Thread {
+
+	/**
+	 * 
+	 */
+	public AgentServer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package jus.aor.mobilagent.kernel;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+
+/**
+ * @author eudes
+ *
+ */
+public class BAMAgentClassLoader extends URLClassLoader {
+
+	public BAMAgentClassLoader(URL[] urls, ClassLoader parent) {
+		super(urls, parent);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
