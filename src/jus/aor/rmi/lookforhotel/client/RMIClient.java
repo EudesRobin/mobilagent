23a52fba518c731f9c0e5cd559a28ecb5e3d66bc
Eudes Robin <eudes.robin@gmail.com>
2015-03-28 14:44:33 +0100

    rmi client side

---
+++ src/jus/aor/rmi/lookforhotel/client/RMIClient.java
@@ -0,0 +1,22 @@
+package jus.aor.rmi.lookforhotel.client;
+
+/**
+ * @author eudes
+ *
+ */
+public class RMIClient {
+
+	/**
+	 * @param args <serveur name> <serveur port> <method name>
+	 */
+	public static void main(String[] args) {
+	
+		if(System.getSecurityManager()==null){
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		
+
+	}
+
+}
